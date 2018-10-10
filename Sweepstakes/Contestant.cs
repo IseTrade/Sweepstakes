f25f9cd6d78f2ecfbaa362eb604b0952fcae81b7
IseTrade <IseTrade@gmail.com>
2018-10-10 13:50:09 -0500

    Added method to get contestant's information.

--- Sweepstakes/Contestant.cs
+++ Sweepstakes/Contestant.cs
@@ -6,22 +6,14 @@ using System.Threading.Tasks;
 
 namespace SweepstakesMarketing
 {
-    class Contestant : ISweepstakes
+    class Contestant
     {
         private string firstName;
         private string lastName;
         private string emailAddress;
         private string registrationNumber;
 
-        public Contestant()
-        {
-            //Default constructor
-        }
 
-        public void GetContestantInfo()
-        {
-
-        }
         /// <summary>
         /// An overloaded constructor to create a contestant with all its required class variables
         /// </summary>
@@ -36,5 +28,20 @@ namespace SweepstakesMarketing
             this.emailAddress = emailAddress;
             this.registrationNumber = registrationNumber;
         }
+
+        public void GetContestantInfo(Contestant contestant)
+        {
+            Console.WriteLine("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Please enter your Email Address: ");
+            string emailAddress = Console.ReadLine();
+            Console.WriteLine("Please enter your Registration Number: ");
+            string registrationNumber = Console.ReadLine();
+            Console.WriteLine("Press any key to continue \n");
+        }
+
+        Dictionary<string, Contestant> dictionaryContestants = new Dictionary<string, Contestant>();
     }
 }
